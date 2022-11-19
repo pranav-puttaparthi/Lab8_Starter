f72e0183c60c160c8952478e4c2405780bca0938
Chris Momil <khoolman1@gmail.com>
2022-11-18 19:15:22 -0800

    Completed all the E2ETests and unit tests (Expose/Explore)

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Lab 8 - Starter
+Names: Pranav Puttaparthi, Jackie Sun
+
+1. Where would you fit your automated tests in your Recipe project development pipeline?
+
+   I would fit them within a Github action that runs whenever code is pushed (choice 1). According to the Intro for this lab, it's a very recent development in software testing practices to automatically test software on certain events, one of them being when code is pushed to a remote repository. Manually running tests locally before pushing code is a very good practice, but it doesn't answer the question of where to fit automated tests. And running them all after all development is completed will leave us way too overwhelmed with all-over-the-place errors, especially if we refrain from manually testing code.
+   
+2. No
+
+3. I would not use a unit test to test the "message" feature of a messaging application. The "message" feature allows the user to write a message, and then send a message to another user. Thus, the whole feature can't be tested with just one unit test, since how individual components interact with each other at an application level cannot be tested.
+
+4. I would use a unit test to test the max message length feature, because this only concerns the writing portion of the messaging feature. So we only need one unit test to check whether or not we can write more than 80 characters.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -2,4 +2,83 @@
 
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
+test('acknowledges 858-231-7042 as a valid phone number', () => {
+    expect(functions.isPhoneNumber('858-231-7042')).toBe(true);
+});
+
+test('acknowledges (619)003-0004 as a valid phone number', () => {
+    expect(functions.isPhoneNumber('(619)003-0004')).toBe(true);
+});
+
+test('acknowledge BILL-HOW as an invalid phone number', () => {
+    expect(functions.isPhoneNumber('BILL-HOW')).toBe(false);
+});
+
+test('acknowledge phillsteak22@fakemail.com as an invalid phone number', () => {
+    expect(functions.isPhoneNumber('phillsteak22@fakemail.com')).toBe(false);
+});
+
+test('acknowledge phillsteak23@fakemail.com as a valid email', () => {
+    expect(functions.isEmail('phillsteak23@fakemail.com')).toBe(true);
+});
+
+test('acknowledge 1@gamers.com as a valid email', () => {
+    expect(functions.isEmail('1@gamers.com')).toBe(true);
+});
+
+test('acknowledge @gmail.com as an invalid email', () => {
+    expect(functions.isEmail('@gmail.com')).toBe(false);
+});
+
+test('acknowledge @.top as an invalid email', () => {
+    expect(functions.isEmail('@.top')).toBe(false);
+});
+
+test('acknowledge StrongPassword123 as a strong password', () => {
+    expect(functions.isStrongPassword('password')).toBe(true);
+});
+
+test('acknowledge Str0ng3rPassw0rd$#@$ as a strong password', () => {
+    expect(functions.isStrongPassword('passw3rd_great')).toBe(true);
+});
+
+test('acknowledge StrongPassword123 as a weak password', () => {
+    expect(functions.isStrongPassword('StrongPassword123')).toBe(false);
+});
+
+test('acknowledge Str0ng3rPassw0rd$#@$ as a weak password', () => {
+    expect(functions.isStrongPassword('Str0ng3rPassw0rd$#@$')).toBe(false);
+});
+
+test('acknowledge 12/12/2022 as a valid date', () => {
+    expect(functions.isDate('12/12/2022')).toBe(true);
+});
+
+test('acknowledge 31/10/1756 as a valid date', () => {
+    expect(functions.isDate('31/10/1756')).toBe(true);
+});
+
+test('acknowledge March 32nd, 2003 as an invalid date', () => {
+    expect(functions.isDate('March 32nd, 2003')).toBe(false);
+});
+
+test('acknowledge February 30th, 1999 as an invalid date', () => {
+    expect(functions.isDate('February 30th, 1999')).toBe(false);
+});
+
+test('acknowledge #256 as a valid color', () => {
+    expect(functions.isHexColor('#256')).toBe(true);
+});
+
+test('acknowledge #FFFFFF as a valid color', () => {
+    expect(functions.isHexColor('#FFFFFF')).toBe(true);
+});
+
+test('acknowledge @#$ as an invalid color', () => {
+    expect(functions.isHexColor('@#$')).toBe(false);
+});
+
+test('acknowledge JJJXXX as an invalid color', () => {
+    expect(functions.isHexColor('JJJXXX')).toBe(false);
+});
 // TODO - Part 2
